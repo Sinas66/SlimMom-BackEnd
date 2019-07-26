afe0bae898d1fde0b1b78209ee981402cbaa4ee0
Sinas66 <Pashkomania@gmail.com>
2019-07-26 14:46:24 +0300

    server init

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,10 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 
+noSuchPageHandler = (req, res) => {
+    res.end(`nooo`)
+
+}
+// router.use(`/api/v1`)
 router.get(`/`, (req, res) => {
     res.end(`Hello World`)
 })
-// .get(`/test`, test())
-
+    // .get(`/test`, test())
+    .get(`*`, noSuchPageHandler)
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -15,16 +15,16 @@ const startServer = port => {
 
     app
         .use(cors())
-        .use(express.urlencoded({ extended: false }))
-        .use(express.json())
-        .use(morgan(':method :url :status :res[content-length] - :response-time ms'))
-        .use(favicon(path.join(__dirname, '../public/images/favicon.ico')))
-        .use('/public', express.static(staticImagePath))
-        .use(router)
-        .use(errorHandler);
-    app.listen(port);
-    console.log('Server was started at http://localhost:' + port);
+        .use(express.urlencoded({ extended: false })) // Добавляем query в url
+        .use(express.json()) // Добавляем body в req
+        .use(morgan(':method :url :status :res[content-length] - :response-time ms')) // В консоле показывает действие
+        .use(favicon(path.join(__dirname, '../public/images/favicon.ico'))) // Добавляем фавикон
+        .use('/public', express.static(staticImagePath)) // Публичная папка
+        .use(`/api/v1`, router) // Путь для нашего роутера
+        .use(errorHandler); // Отлавливаем ошибки сервера
+    app.listen(port); // Порт на котором работает сервер
 
+    console.log('Server was started at http://localhost:' + port);
 }
 
 module.exports = startServer;
