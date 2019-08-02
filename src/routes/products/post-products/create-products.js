9a612ed854b9483459ccc7244ac01da2fe5e1340
Viktor Bulvarenko <viktor@bulvarenko.pw>
2019-08-02 20:20:15 +0300

    add new xlsx to json

--- src/routes/products/post-products/create-products.js
+++ src/routes/products/post-products/create-products.js
@@ -1,116 +1,38 @@
-// const multer = require('multer');
-// const path = require('path');
-// eslint-disable-next-line node/prefer-global/buffer
-const fs = require(`fs`);
-// const util = require('util');
-
-
+const path = require('path');
 // const Ingredient = require(`../../../model/products.model`);
-const readXlsxFile = require('read-excel-file/node');
-
 // const exelTmpFolder = `tmp/Ingredients-exel`;
-// const exelFolder = `Ingredients-exel`;
-// const TMP_EXEL_FOLDER = path.join(
-// 	__dirname,
-// 	'../../../../private',
-// 	exelTmpFolder,
-// );
-// const EXEL_FOLDER = path.join(__dirname, '../../../../private', exelFolder);
-
-// const renameFile = util.promisify(fs.rename);
-
-// const storage = multer.diskStorage({
-// 	destination(req, file, cb) {
-// 		cb(null, TMP_EXEL_FOLDER);
-// 	},
-// 	filename(req, file, cb) {
-// 		cb(null, file.originalname);
-// 	},
-// });
-
-const saveInDB = () => {
-	const schema = {
-		Название: {
-			prop: 'ru',
-			type: String,
-		},
-		Назва: {
-			prop: 'ua',
-			type: String,
-		},
-		'Кал, ккал': {
-			prop: 'calories',
-			type: Number,
-		},
-		'Категория продуктов': {
-			prop: 'categories',
-			type: String,
-		},
-	};
-
-	return readXlsxFile(fs.createReadStream('./uploads/bd_products.xlsx'), { schema })
-		.then(({ rows, errors }) => {
-			console.log({ rows, errors });
-
-			// return Ingredient.insertMany(rows, {
-			// 	bypassDocumentValidation: true,
-			// 	ordered: false,
-			// 	rawResult: false,
-			// })
-			// 	.then(data => {
-			// 		const saved = data.map(el => {
-			// 			console.log(`this.name: `, this.name);
-
-			// 			const item = {
-			// 				// eslint-disable-next-line no-underscore-dangle
-			// 				id: el._id,
-			// 				name: el.name,
-			// 				protein: el.protein,
-			// 				fat: el.fat,
-			// 				сarbohydrate: el.сarbohydrate,
-			// 				energy: el.energy,
-			// 			};
-			// 			return item;
-			// 		});
-
-			// 		const respData = {
-			// 			savedData: saved,
-			// 			// savedRows: rows,
-			// 			dataWithError: errors,
-			// 		};
-			// 		return respData;
-			// 	})
-			// 	.catch(err => {
-			// 		return err;
-			// 	});
-		})
-		.catch(err => {
-			return err;
-		});
-};
+const productsFile = path.join(__dirname, '../../../../uploads/bd_products.xlsx');
 
 const createProducts = (req, res) => {
-	const fileObject = req.file;
+	// multer - читає файл і записує його у папку uploads
+	// в req.file - беремо назву файлу
+	
+	const excelToJson = require('convert-excel-to-json');
 
-	console.log(fileObject);
-	res.json({ ggog: 'googo' });
-	// const time = new Date().toLocaleString().replace(/:/g, `-`);
+	const result = excelToJson({
+			sourceFile: productsFile,
+			header:{
+					rows: 2
+			},
+			sheets: ['База данных продуктов']
+	});
+	const getFiledArray = result['База данных продуктов'].map(product => ({
+		title: {
+			ru: product.A,
+			ua: product.E,
+		},
+		calories: product.F,
+		categories: [product.G],
+		groupBloodNotAllowed: {
+			1: `${product.H === 'НЕЛЬЗЯ'}`,
+			2: `${product.I === 'НЕЛЬЗЯ'}`,
+			3: `${product.J === 'НЕЛЬЗЯ'}`,
+			4: `${product.K === 'НЕЛЬЗЯ'}`
+		}
+	}));
+	console.log(result)
+	res.json({ getFiledArray });
 
-	// const sendError = err => {
-	// 	res.status(404).json({ err });
-	// };
-
-	// const sendResp = data => {
-	// 	res.json({
-	// 		status: `success`,
-	// 		result: data,
-	// 	});
-	// };
-
-	saveInDB();
-	// .then(saveInDB(fileObject))
-	// .then(sendResp)
-	// .catch(sendError);
 };
 
 module.exports = createProducts;
