ca80197084123b9b5b6a2185acc8cd75d72a0e84
Sinas66 <Pashkomania@gmail.com>
2019-08-05 16:10:23 +0300

    add more checks in auth middleware

--- src/middleware/check-user-and-pas.js
+++ src/middleware/check-user-and-pas.js
@@ -1,4 +1,4 @@
-const errors = require(`../config.js`).errors.auth;
+const { errors } = require(`../config.js`);
 
 const checkBodyFornicknameAndPass = (req, res, next) => {
 	const user = req.body;
@@ -35,20 +35,75 @@ const checkBodyFornicknameAndPass = (req, res, next) => {
 	};
 
 	if (req.headers[`content-type`] !== `application/json`) {
-		sendError(errors.onlyJson); // Пропускаем только json
+		sendError(errors.auth.onlyJson); // Пропускаем только json
 		return;
 	}
 	if (!user.nickname && !user.password) {
-		sendError(errors.passAndUserRequired); // Если нет nickname и password то шлем ошибку
+		sendError(errors.auth.passAndUserRequired); // Если нет nickname и password то шлем ошибку
 		return;
 	}
 	if (!user.nickname) {
-		sendError(errors.userRequired); // Если нет nickname то шлем ошибку
+		sendError(errors.auth.userRequired); // Если нет nickname то шлем ошибку
 		return;
 	}
 	if (!user.password) {
-		sendError(errors.passRequired); // Если нет Password то шлем ошибку
+		sendError(errors.auth.passRequired); // Если нет Password то шлем ошибку
 		return;
+	}
+	if (user.userData) {
+		const { height, currentWeight, age, desiredWeight } = req.body.userData;
+		if (!age && !height && !currentWeight && !desiredWeight) {
+			sendError(errors.calc.allFieldsAreRequired);
+		}
+		if (!age) {
+			sendError(errors.calc.ageIsRequired);
+			return;
+		}
+		if (!height) {
+			sendError(errors.calc.heightIsRequired);
+			return;
+		}
+		if (!currentWeight) {
+			sendError(errors.calc.currentWeightIsRequired);
+			return;
+		}
+		if (!desiredWeight) {
+			sendError(errors.calc.desiredWeight);
+			return;
+		}
+		if (
+			typeof desiredWeight !== 'number' ||
+			typeof currentWeight !== 'number'
+		) {
+			sendError(errors.calc.weightNum);
+			return;
+		}
+		if (!Number.isInteger(age) || !Number.isInteger(height)) {
+			sendError(errors.calc.ageHeightCeilNum);
+			return;
+		}
+		if (age < 1 || age > 99) {
+			sendError(errors.calc.ageRange);
+			return;
+		}
+		if (height < 1 || height > 230) {
+			sendError(errors.calc.heightRange);
+			return;
+		}
+		if (
+			currentWeight < 1 ||
+			currentWeight > 230 ||
+			desiredWeight < 1 ||
+			desiredWeight > 230
+		) {
+			sendError(errors.calc.weightRange);
+			return;
+		}
+
+		if (currentWeight < desiredWeight) {
+			sendError(errors.calc.onlyLosingWeight);
+			return;
+		}
 	}
 
 	next();
