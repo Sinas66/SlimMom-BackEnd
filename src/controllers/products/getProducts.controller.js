fced53b851f386c7463e4134247efa8849de9035
Sinas66 <Pashkomania@gmail.com>
2019-08-09 16:28:08 +0300

    getProducts add value & color && label

--- src/controllers/products/getProducts.controller.js
+++ src/controllers/products/getProducts.controller.js
@@ -1,6 +1,10 @@
 const Products = require(`../../model/products.model`);
 
 const getProducts = (req, res) => {
+	// const { userId } = req;
+	const userGroupBlood = Number(req.params.groupBlood);
+	console.log(userGroupBlood);
+
 	const sendResponse = products => {
 		res.json({
 			status: 'success',
@@ -17,6 +21,20 @@ const getProducts = (req, res) => {
 	};
 
 	Products.find()
+		.then(products => {
+			const newProducts = products.map(el => {
+				return {
+					...el._doc,
+					value: el._doc._id,
+					label: el._doc.title.ru,
+					color:
+						el._doc.groupBloodNotAllowed[userGroupBlood] === true
+							? 'red'
+							: 'black',
+				};
+			});
+			return newProducts;
+		})
 		.then(sendResponse)
 		.catch(sendError);
 };
